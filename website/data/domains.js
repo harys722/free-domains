7bf4f744ef9ad4689de2e951fa7fb15a1f287560
- HARYs - <174037813+harys722@users.noreply.github.com>
2025-06-20 18:31:56 +0500

    All domains list

---
+++ website/data/domains.js
@@ -0,0 +1,176 @@
+// domains.js - Domain data for Free Domains website
+const domainsData = [
+    {
+        name: "is-a.dev",
+        url: "https://www.is-a.dev/",
+        description: "Grab your own sweet-looking '.is-a.dev' subdomain.",
+        repo: "is-a-dev/register",
+        available: true
+    },
+    {
+        name: "js.org",
+        url: "https://js.org/",
+        description: "Dedicated to JavaScript and its awesome community since 2015",
+        repo: "js-org/js.org",
+        available: true
+    },
+    {
+        name: "Open Domains",
+        url: "https://open-domains.net",
+        description: "Free subdomains for personal sites, open-source projects, and more. Offers is-cool.dev, is-a-fullstack.dev, is-local.org, is-not-a.dev, and localplayer.dev.",
+        repo: "open-domains/register",
+        available: true
+    },
+    {
+        name: "Domains Project",
+        url: "https://getyourfree.space/",
+        description: "Free developer subdomains under owns.it.com, with more domains coming soon.",
+        repo: "domainsproject/register",
+        available: true
+    },
+    {
+        name: "foo.ng",
+        url: "https://foo.ng/",
+        description: "Free foo.ng subdomains for everyone.",
+        repo: "Pokymon/foo.ng",
+        available: true
+    },
+    {
+        name: "cluster.ws & wip.la",
+        url: "https://cluster.ws/",
+        description: "Get a cluster.ws & wip.la domain for free and instantly",
+        repo: "Olivr/free-domain",
+        available: true
+    },
+    {
+        name: "Open Host",
+        url: "https://registry.openhost.uk/",
+        description: "A free subdomain service offering subdomains on prvcy.page, 16-b.it, 32-b.it, 64-b.it, pride.moe and pride.ngo.",
+        repo: null,
+        available: true
+    },
+    {
+        name: "thedev.id",
+        url: "https://thedev.id/",
+        description: "An identity for developers on the web.",
+        repo: "thedev-id/thedev.id",
+        available: true
+    },
+    {
+        name: "is-a-good.dev",
+        url: "https://is-a-good.dev/",
+        description: "Register for your is-a-good.dev domain!",
+        repo: "is-a-good-dev/register",
+        available: true
+    },
+    {
+        name: "is-truly-a.pro",
+        url: "https://is-truly-a.pro/",
+        description: "Free is-truly-a.pro subdomains, available for everyone!",
+        repo: "is-truly-a-pro/register",
+        available: true
+    },
+    {
+        name: "is-not.cool",
+        url: "https://is-not.cool/",
+        description: "Get your own 'is-not.cool' subdomain!",
+        repo: "is-not-cool/registration",
+        available: true
+    },
+    {
+        name: "js.cool",
+        url: "https://js.cool/",
+        description: "Provide free subdomains for Chinese open source projects (since August 2020), now support Vercel!",
+        repo: "willin/domain",
+        available: true
+    },
+    {
+        name: "ngo.us",
+        url: "https://nic.ngo.us/",
+        description: "Free subdomains exclusively for nonprofits, nongovernmental organizations (NGOs) and initiatives.",
+        repo: "ngo-us/registry",
+        available: true
+    },
+    {
+        name: "us.kg",
+        url: "https://nic.us.kg/",
+        description: "Free subdomain service run by the nonprofit DigitalPlat Foundation, supported by the Hack Foundation.",
+        repo: null,
+        available: true
+    },
+    {
+        name: "pp.ua",
+        url: "https://pp.ua/",
+        description: "Free pp.ua subdomains.",
+        repo: null,
+        available: true
+    },
+    {
+        name: "zone.id",
+        url: "https://zone.id/",
+        description: "Zone.ID is a premium subdomain provider as an alternative to the traditional .id TLD.",
+        repo: null,
+        available: true
+    },
+    {
+        name: "Is-Cool-Me",
+        url: "https://registry.openhost.uk/",
+        description: "Free subdomains for personal sites, open-source projects, and more, offering subdomains on is-epic.me and is-awsm.tech.",
+        repo: null,
+        available: true
+    },
+    {
+        name: "nyc.mn",
+        url: "https://dot.nyc.mn/",
+        description: "Stand out in the digital world with a .NYC.mn domain, crafted to showcase your unique connection to New York City.",
+        repo: null,
+        available: false
+    },
+    {
+        name: "is-an.app",
+        url: "https://is-an.app/",
+        description: "🌐 DNS configuration for some of my domains. (Not available anymore)",
+        repo: "tarampampam/free-domains",
+        available: false
+    },
+    {
+        name: "is-a.co",
+        url: "https://is-a.co/",
+        description: "🌐 Grab your own subdomain for free Follow the steps listed below to get your own subdomain!",
+        repo: "PythonicBoat/is-a.co",
+        available: false
+    },
+    {
+        name: "withcoding.me",
+        url: "http://withcoding.me/",
+        description: "Free subdomain for everyone.",
+        repo: "JastinXyz/withcoding.me",
+        available: false
+    },
+    {
+        name: "is-amaz.ing",
+        url: "https://is-amaz.ing",
+        description: "Register your is-amaz.ing project domain here!",
+        repo: "is-amazing/register",
+        available: false
+    },
+    {
+        name: "tilde.tk",
+        url: "https://tilde.tk/",
+        description: "In need of a cool subdomain? And want it to be your own UNIX home directory? Then tilde.tk (~.tk) is right for you",
+        repo: "youngchief-btw/tilde.tk",
+        available: false
+    },
+    {
+        name: "merahputih.moe",
+        url: "https://Merahputih.moe/",
+        description: "Are you weeb developer looking for free webspace to host your project? Get free subdomain merahputih.moe UwU",
+        repo: "ScathachGrip/merahputih.moe",
+        available: false
+    }
+];
+
+// Export for use in other files
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = domainsData;
+}
